f25dede1792dd4ad1b55e48aa408cfce71151975
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2024-02-05 08:39:04 +0100

    Update BACARDI labels
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- src/pylim/helpers.py
+++ src/pylim/helpers.py
@@ -118,11 +118,15 @@ norms = dict(t=colors.TwoSlopeNorm(vmin=200, vcenter=235, vmax=280), clwc=colors
              od_int=colors.LogNorm(vmax=10), scat_od_int=colors.LogNorm())
 
 # plotting dictionaries for BACARDI
-bacardi_labels = dict(F_down_solar=r"$F_{\downarrow, solar}$", F_down_terrestrial=r"$F_{\downarrow, terrestrial}$",
-                      F_up_solar=r"$F_{\uparrow, solar}$", F_up_terrestrial=r"$F_{\uparrow, terrestrial}$",
-                      F_net_solar=r"$F_{net, solar}$", F_net_terrestrial=r"$F_{net, terrestrial}$",
-                      CRE_solar=r"CRE$_{solar}$", CRE_terrestrial=r"CRE$_{terrestrial}$",
-                      CRE_total=r"CRE$_{total}$")
+bacardi_labels = dict(F_down_solar=r"$F_{\downarrow, \mathrm{solar}}$",
+                      F_down_terrestrial=r"$F_{\downarrow, \mathrm{terrestrial}}$",
+                      F_up_solar=r"$F_{\uparrow, \mathrm{solar}}$",
+                      F_up_terrestrial=r"$F_{\uparrow, \mathrm{terrestrial}}$",
+                      F_net_solar=r"$F_{\mathrm{net, solar}}$",
+                      F_net_terrestrial=r"$F_{\mathrm{net, terrestrial}}$",
+                      CRE_solar=r"CRE$_{\mathrm{solar}}$",
+                      CRE_terrestrial=r"CRE$_{\mathrm{terrestrial}}$",
+                      CRE_total=r"CRE$_{\mathrm{total}}$")
 
 cb_color_cycles = dict(
     cartocolor=["#88CCEE", "#CC6677", "#DDCC77", "#332288", "#AA4499", "#44AA99", "#999933",
